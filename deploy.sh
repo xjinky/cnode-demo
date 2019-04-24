09a1868fe6fc9ee84c74a1aa28b0c8be8a062fa3
xjinky <xjinky@163.com>
2019-04-24 13:31:22 +0800

    vue-cli deploy

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+# deploy.sh
+# 错误时停止
+set -e
+
+# 打包
+npm run build
+
+# 进入目标文件夹
+cd dist
+
+# 提交到本地仓库
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 提交到 https://github.com:nusr/resume-vue 项目的 gh-pages 分支
+git remote add origin https://github.com/xjinky/cnode-demo.git
+
+git push -u origin master
+
+cd -
